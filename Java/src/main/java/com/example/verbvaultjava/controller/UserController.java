8b5ed3bc5ecd2948b0b810b124ca0c43f07c4ddd
Tomasz Hamerla <tomaszhamerla@interia.pl>
2023-10-30 21:08:22 +0100

    added user worlds einpoint

---
+++ Java/src/main/java/com/example/verbvaultjava/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.verbvaultjava.controller;
+
+import com.example.verbvaultjava.model.dto.UserResponse;
+import com.example.verbvaultjava.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+private final UserService userService;
+    @GetMapping
+    public ResponseEntity<List<UserResponse>>getUsersWithWorlds(){
+
+        return ResponseEntity.ok(userService.getUsersResponse());
+    }
+}
