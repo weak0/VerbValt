68b47e37489e98c2ed50d3d07f3d404eb24935fc
unknown <tomaszhamerla@interia.pl>
2023-11-26 14:30:32 +0100

    26-11_added column progress to user_course

---
+++ Java/src/main/java/com/example/verbvaultjava/model/course/UserCourse.java
@@ -0,0 +1,21 @@
+package com.example.verbvaultjava.model.course;
+
+import com.example.verbvaultjava.model.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user_course")
+@Data
+public class UserCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+    private int progress;
+}
