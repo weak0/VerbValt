55858df504b4ffce5390b09eb50514ae77d10687
Maciej Gorzela <maciej.gorzela89@gmail.com>
2023-12-02 11:37:54 +0100

    02-12_Fisnished course details

--- React/src/Home/Courses/CourseDetails.jsx
+++ React/src/Home/Courses/CourseDetails.jsx
@@ -1,24 +1,44 @@
 import React from 'react'
+import './Courses.css'
 
 const CourseDetails = ({courseDetails}) => {
     const { courseSentences, courseWords} = courseDetails
     console.log(courseSentences, courseWords)
   return (
    <>
-    <h2 className='section-tittle'>Courses Details</h2>
-    <div className='details-header'>
-        <p>Words</p>
-        <p>Translation</p>
+    <h2 className='section-tittle '>Courses Details</h2>
+    <div className='details-words-main'>
+    <div className='details-header details-table'>
+        <h3>Words</h3>
+        <h3>Translation</h3>
     </div>
     {courseWords.map(word => {
         return (
-            <div key={word.id} className='details-row'>
-                <p>{word.word}</p>
+            <div key={word.id} className='details-row  details-table'>
+                <p>{word.foreignWord}</p>
                 <p>{word.translation}</p>
             </div>
         )
     }
+    
     )}
+    </div>
+    <div className='details-words-main'>
+    <div className='details-header details-table'>
+        <h3>Sentence</h3>
+        <h3>Translation</h3>
+    </div>
+    {courseSentences.map(word => {
+        return (
+            <div key={word.id} className='details-row  details-table'>
+                <p>{word.foreignSentence}</p>
+                <p>{word.translation}</p>
+            </div>
+        )
+    }
+    
+    )}
+    </div>
    </>
   )
 }
