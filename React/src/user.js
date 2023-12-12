f817554875bc8b1283258b96293ec466d7f309a1
weak0 <maciej.gorzela89@gmail.com>
2023-12-12 23:37:54 +0100

    Add new icons and update sidebar functionality

--- React/src/user.js
+++ React/src/user.js
@@ -1,7 +1,36 @@
+import {jwtDecode} from "jwt-decode";
+
+const testToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYWNpZWtAZXhhbXBsZS54ZCIsImp0aSI6IjExIiwiaWF0IjoxNzAyNDEyOTIxLCJleHAiOjE3MDI0OTkzMjF9.KoAEChD_nG_7rnwF66mapWlUkl7ONvT9YSsS9FjYI6c"
+
+const decodeToken = (token) => {
+    const decoded = jwtDecode(token);
+    console.log(decoded)
+    return decoded;
+}
+
 export const testuser = {
-    id: 4,
-    name: 'maciek',
-    email: 'email',
-    toke: '1234567890', 
-    roleId: 1,
-} 
+    token: testToken,
+    id: decodeToken(testToken).jti,
+}
+
+export const fetchData = async (url, method, body = null) => {
+    const options = {
+        method: method,
+        headers: {
+            'Authorization': `Bearer ${testuser.token}`,
+            'Content-Type': 'application/json'
+        },
+    };
+
+    if (method !== "GET" && body) {
+        options.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`http://127.0.0.1:8000/${url}`, options);
+
+    if (response.ok) {
+        return response.json();
+    } else {
+        alert("Error");
+    }
+}
--- React/src/utils.js
+++ React/src/utils.js
@@ -1,3 +1,15 @@
 export function isEmpty(obj) {
     return Object.keys(obj).length === 0 && obj.constructor === Object;
 }
+export const learnTypeEnum = {
+    Default: 0,
+    Words: 1,
+    Sentences: 2,
+}
+
+export const pageEnum = {
+    Courses: 0,
+    Words: 1,
+    Learn: 2,
+  }
+  
