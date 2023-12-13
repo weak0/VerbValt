f29241aba5a135ac8c0df9a6bf3d1ec22aff2cbe
Tomasz Hamerla <tomaszhamerla@interia.pl>
2023-12-13 18:53:34 +0100

    13-12_added nickanme to registerRequest

--- Java/src/main/java/com/example/verbvaultjava/auth/RegisterRequest.java
+++ Java/src/main/java/com/example/verbvaultjava/auth/RegisterRequest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    private String nickName;
     private String firstName;
     private String email;
     private String password;
