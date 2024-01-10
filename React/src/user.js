7283071db82a39b953a7ac1a57fb33e8d0534bed
weak0 <maciej.gorzela89@gmail.com>
2024-01-10 16:15:06 +0100

    Refactor user.js to use localStorage for token

--- React/src/user.js
+++ React/src/user.js
@@ -1,16 +1,13 @@
 import {jwtDecode} from "jwt-decode";
 
-const testToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYWNpZWtAZXhhbXBsZS54ZCIsImp0aSI6IjExIiwiaWF0IjoxNzAyNDI3MDE1LCJleHAiOjE3MDI1MTM0MTV9.VQnnvMws4oQ5dzemB9kDC2-CdQC3eOX6adRQ-5bzxac"
-
 const decodeToken = (token) => {
     const decoded = jwtDecode(token);
-    console.log(decoded)
     return decoded;
 }
 
-export const testuser = {
-    token: testToken,
-    id: decodeToken(testToken).jti,
+export const user  = {
+    token: localStorage.getItem("token"),
+    id: decodeToken(token).jti,
 }
 
 export const fetchData = async (url, method, body = null) => {
